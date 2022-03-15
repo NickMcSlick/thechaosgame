00b50ca29d4800d2328ad28e5016331d8fe3f06a
NickMcSlick <bryce.paubel@gmail.com>
2022-03-15 11:25:40 -0500

    Added more unit tests

--- tests/tests.js
+++ tests/tests.js
@@ -5,6 +5,16 @@
 
 /***** TESTS *****/
 
+// Dummy variables used for the tests
+let dummyCanvas = document.createElement("canvas");
+let dummyGL = dummyCanvas.getContext("webgl");
+let dummyEvent = new MouseEvent("click", {clientX: 400, clientY: 400});
+Object.defineProperty(dummyEvent, "target", {writable: false, value: dummyCanvas});
+
+// Size the canvas for standard testing
+dummyCanvas.width = 500;
+dummyCanvas.height = 500;
+
 // Test if the fragment shader exists
 function fShaderTest() {
     if (FSHADER) {
@@ -45,6 +55,29 @@ function testElementExists(name) {
     }
 }
 
+// Test that the mouseover event handler is properly translating coordinates
+function testMouseOver() {
+    let dummyMouse = new Point();
+    updateMousePosition(dummyEvent, dummyMouse, dummyCanvas);
+    if (Math.abs(dummyMouse.x) > 1 || Math.abs(dummyMouse.y) > 1) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+// Test that the point placing event properly translates coordinates
+function testMouseDown() {
+    let dummyMouse = new Point();
+    let dummyPoints = [];
+    placePoint(dummyEvent, dummyMouse, dummyPoints, dummyCanvas);
+    if (Math.abs(dummyPoints[0].x) > 1 || Math.abs(dummyPoints[0].y) > 1) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
 // Run the tests
 function runTests() {
     console.log("Fragment Shader: " + fShaderTest());
@@ -56,4 +89,6 @@ function runTests() {
     console.log("Game board exists: " + testElementExists("game_board"));
     console.log("Inner game div exists: " + testElementExists("inner_game"));
     console.log("Controls area exists: " + testElementExists("controls"));
+    console.log("Mouse over properly translates: " + testMouseOver());
+    console.log("Mouse click properly translates: " + testMouseDown());
 }
