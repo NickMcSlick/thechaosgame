d6e0da75f20a2200913b7aad15d9dd096fb53386
Michael Schall <michael.schall@answers.com>
2022-04-13 14:07:41 -0500

    Game Page bug fix and mainPage Tutorial button is now preselected on pageload

--- js/mainPage.js
+++ js/mainPage.js
@@ -29,66 +29,6 @@ function indexTutorialButton()
     location.assign("./html/tutorial.html")
 }
 
-function indexTutorialText()
-{
-    location.assign("./html/tutorial.html")
-}
-
-function indexPlayText()
-{
-    location.assign("./html/game.html")
-}
-
-var tutorialSelected = true
-var playSelected = false
-
-document.addEventListener('keyup', (event) =>
-{
-    var text
-    if((event.code==='ArrowDown' || event.code === 'ArrowUp') && tutorialSelected === false && playSelected===true)
-    {
-        document.getElementById('indexPlayText').classList.remove('highlightedText')
-        document.getElementById('indexTutorialText').classList.add('highlightedText')
-        text = document.getElementById('indexPlayText')
-        text.textContent = "Play"
-        text = document.getElementById('indexTutorialText')
-        text.textContent = "> "+text.innerText+" <"
-        tutorialSelected = true
-        playSelected = false
-    }
-    else if((event.code==='ArrowDown' || event.code === 'ArrowUp') && tutorialSelected === true && playSelected===false)
-    {
-        document.getElementById('indexTutorialText').classList.remove('highlightedText')
-        document.getElementById('indexPlayText').classList.add('highlightedText')
-        text = document.getElementById('indexTutorialText')
-        text.textContent = "Tutorial"
-        text = document.getElementById('indexPlayText')
-        text.textContent = "> "+text.innerText+" <"
-        playSelected = true
-        tutorialSelected = false
-    }
-    else if(event.code==='ArrowDown' || event.code === 'ArrowUp')
-    {
-        document.getElementById('indexTutorialText').classList.add('highlightedText')
-        document.getElementById('indexPlayText').classList.remove('highlightedText')
-        text = document.getElementById('indexPlayText')
-        text.textContent = "Play"
-        text = document.getElementById('indexTutorialText')
-        text.textContent = "> "+text.innerText+" <"
-        tutorialSelected = true
-        playSelected = false
-    }
-    
-    if((event.code==='Enter') && tutorialSelected === false && playSelected===true)
-    {
-        indexPlayText()
-    }
-    else if((event.code==='Enter') && tutorialSelected === true && playSelected===false)
-    {
-        indexTutorialText()
-    }
-})
-
 /* IF THE TUTORIAL SESSION OBJECT HAS NOT BEEN CREATED, DISPLAY THE TUTORIAL MESSAGE */
 /* IT SHOULD BE NOTED THAT THIS WON'T WORK FOR EVERY BROWSER DUE TO COOKIE RESTRICTIONS */
 function updateTutorialMessageVisibility() {
